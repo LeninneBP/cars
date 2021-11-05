5c34709e986f5d9832d7806e5893b6ef19298382
LeninneBP <leninne.bpirovano@gmail.com>
2021-11-05 16:43:17 -0400

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
+import ImagePreview from 'primevue/imagepreview';
+
+
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
 
 Vue.config.productionTip = false
+
+Vue.use(PrimeVue)
+
+Vue.component('Dialog', Dialog);
+Vue.component('Button', Button);
+Vue.component('ImagePreview', ImagePreview);
 
 new Vue({
   render: h => h(App),
