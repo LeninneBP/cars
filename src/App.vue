f6f2c9dd83595cc3384a901e6d9d232778325d4f
LeninneBP <leninne.bpirovano@gmail.com>
2021-11-05 17:04:22 -0400

    terminar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header class="header">
+
+    </header>
+  
+    <Home/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Home from './components/home/Home.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+// export default{
+
+//   name: 'Home',
+//   components:{
+//     'Home':  Home
+//   }
+// }
+
 </script>
 
 <style>
+*{
+  padding: 0;
+  margin: 0;
+
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.header{
+  width: 100%;
+  height: 100px;
+  background-color: #000000;
+
+
+
 }
 </style>
