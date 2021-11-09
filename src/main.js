57af87c40052dca2a5f5bcfbab790cfa7f26e529
LeninneBP <leninne.bpirovano@gmail.com>
2021-11-09 14:11:06 -0400

    tomando forma

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import PrimeVue from 'primevue/config';
 import VueRouter from 'vue-router';
 import router from '../router';
 
-
+import Card from 'primevue/card';
+import Galleria from 'primevue/galleria';
 import Dialog from 'primevue/dialog';
 import Button from 'primevue/button';
 import ImagePreview from 'primevue/imagepreview';
@@ -19,7 +20,8 @@ Vue.config.productionTip = false
 Vue.use(PrimeVue)
 Vue.use(VueRouter);
 
-
+Vue.component('Card', Card);
+Vue.component('Galleria', Galleria);
 Vue.component('Dialog', Dialog);
 Vue.component('Button', Button);
 Vue.component('ImagePreview', ImagePreview);
