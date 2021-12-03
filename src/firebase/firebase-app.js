461e43c3328e815d24c116aec7f8780cde762198
LeninneBP <leninne.bpirovano@gmail.com>
2021-12-03 16:40:51 -0400

    fire base

---
+++ src/firebase/firebase-app.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDrjeI797cUdDNDKebKbLPe3jOcHAVGgZQ",
+    authDomain: "cars-5aee3.firebaseapp.com",
+    projectId: "cars-5aee3",
+    storageBucket: "cars-5aee3.appspot.com",
+    messagingSenderId: "865016878461",
+    appId: "1:865016878461:web:829b3c3b7dd9df0e382814",
+    measurementId: "G-748C3NH5Y2"
+
+}
